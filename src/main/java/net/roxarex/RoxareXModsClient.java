80581eb348c0cd8f74866b769b406e6668ddc39c
RoxareX <ticermark@gmail.com>
2026-03-08 11:23:51 +0200

    Initial commit

---
+++ src/main/java/net/roxarex/RoxareXModsClient.java
@@ -0,0 +1,8 @@
+package net.roxarex;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class RoxareXModsClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {}
+}
